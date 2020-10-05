f81a018aaa9e840101a8aaffe7f8e71bd81b7663
Anurag Narayan <narayan@narayan-mbp.local>
2020-10-05 14:13:24 +0530

    Fixes type in docker build

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,28 @@
+echo "BUILDING multi-client"
 docker build -t nayaran/multi-client:latest -t nayaran/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t nayaran/multi-server:latest -t nayaran/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t nayaran/multi-worker:latest -t nayaran/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+echo "BUILDING multi-server"
+docker build -t nayaran/multi-server:latest -t nayaran/multi-server:$SHA -f ./server/Dockerfile ./server
+
+echo "BUILDING multi-worker"
+docker build -t nayaran/multi-worker:latest -t nayaran/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+echo "UPLOADING multi-client"
 docker push nayaran/multi-client:latest
 docker push nayaran/multi-client:$SHA
+
+echo "UPLOADING multi-server"
 docker push nayaran/multi-server:latest
 docker push nayaran/multi-server:$SHA
+
+echo "UPLOADING multi-worker"
 docker push nayaran/multi-worker:latest
 docker push nayaran/multi-worker:$SHA
 
-kubectl apply k8s
+echo "UPDATING KUBERNETES"
+kubectl apply -f k8s
+
+echo "UPDATING KUBERNETES - using latest images"
 kubectl set image deployments/server-deployment server=nayaran/multi-server:$SHA
 kubectl set image deployments/client-deployment client=nayaran/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=nayaran/multi-worker:$SHA
